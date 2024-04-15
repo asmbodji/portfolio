706f94e0be44b4fc779e8fc7fe98b989a4ae661c
asmbodji <164932520+asmbodji@users.noreply.github.com>
2024-04-15 19:07:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Technicien informatique industrielle","Technicien vision industrielle","Dev web"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
