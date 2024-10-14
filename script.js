dad434b597e2ca228ad59b6b492625c506fa9126
asmbodji <164932520+asmbodji@users.noreply.github.com>
2024-10-14 15:33:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ var typed = new Typed(".typing",{
     BackSpeed:60,
     loop:true
 })
+
+
+
+
+// Add this to your script.js for toggling navigation on small screens
+document.querySelector('.nav-toggler').addEventListener('click', () => {
+    document.querySelector('.aside').classList.toggle('open');
+    document.querySelector('.nav-toggler').classList.toggle('active');
+});
+
+
+
+document.querySelector('.nav-toggler').addEventListener('click', () => {
+    document.querySelector('.aside').classList.toggle('open');
+    document.body.classList.toggle('menu-open');
+});
